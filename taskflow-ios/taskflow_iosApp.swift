8126637f4f7a3b3866812e152926f5149916c52e
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-05-05 01:10:38 -0400

    Initial Commit

---
+++ taskflow-ios/taskflow_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskflow_iosApp.swift
+//  taskflow-ios
+//
+//  Created by Howard Thomas on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct taskflow_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
