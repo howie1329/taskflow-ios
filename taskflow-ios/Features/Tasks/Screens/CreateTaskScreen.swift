e9a627f5abcea6773f35c984108e7dcc5293e3f8
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-05-06 16:53:04 -0400

    Add Create Task Screen UI
    - Introduced `CreateTaskScreen` to allow users to create new tasks.
    - Implemented a simple UI with a text label indicating the purpose of the screen.
    - Added preview functionality for testing the screen in isolation.

---
+++ taskflow-ios/Features/Tasks/Screens/CreateTaskScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTaskScreen.swift
+//  taskflow-ios
+//
+//  Created by Howard Thomas on 5/6/25.
+//
+
+import SwiftUI
+
+struct CreateTaskScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTaskScreen()
+}
