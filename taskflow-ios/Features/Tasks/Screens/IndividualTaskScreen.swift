61c4f59b3e8826dd89d62e7cd319e8249e0ecddf
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-05-06 14:38:36 -0400

    Add Individual Task Screen UI
    - Implemented `IndividualTaskScreen` to display detailed information for a single task, including title, description, due date, priority, and categories.
    - Enhanced UI with a scrollable layout and visual indicators for task completion and priority.
    - Added a helper function to determine color representation for task priority.
    - Included a preview for testing the screen with sample task data.

---
+++ taskflow-ios/Features/Tasks/Screens/IndividualTaskScreen.swift
@@ -0,0 +1,18 @@
+//
+//  IndividualTaskScreen.swift
+//  taskflow-ios
+//
+//  Created by Howard Thomas on 5/6/25.
+//
+
+import SwiftUI
+
+struct IndividualTaskScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndividualTaskScreen()
+}
