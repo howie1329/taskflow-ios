577097ce9a3726d76b01bd370b4b12109c1ad205
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-05-06 14:59:26 -0400

    Add Task Filter Sheet UI Component
    - Introduced `TaskFilterSheet` view to allow users to filter tasks by priority.
    - Implemented a segmented picker for selecting task priority and buttons for applying or resetting filters.
    - Added preview functionality for testing the filter sheet with sample data.

---
+++ taskflow-ios/Features/Tasks/Screens/TaskFilterSheet.swift
@@ -0,0 +1,18 @@
+//
+//  TaskFilterSheet.swift
+//  taskflow-ios
+//
+//  Created by Howard Thomas on 5/6/25.
+//
+
+import SwiftUI
+
+struct TaskFilterSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskFilterSheet()
+}
