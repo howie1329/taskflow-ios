052dc34efaf886f6ea042716c1e86aa98904ee96
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-07-02 07:18:17 -0400

    reseting swift app

--- taskflow-ios/ContentView.swift
+++ taskflow-ios/ContentView.swift
@@ -6,19 +6,13 @@
 //
 
 import SwiftUI
-import Clerk
 
 struct ContentView: View {
     @State private var isLoggin = false
-    @Environment(Clerk.self) private var clerk
     
     var body: some View {
         VStack{
-            if let user = clerk.user {
-                TabUIView()
-            } else {
-                TabUIView()
-            }
+            Text("Here is a SwiftUI app")
         }
     }
 }
--- taskflow-ios/taskflow_iosApp.swift
+++ taskflow-ios/taskflow_iosApp.swift
@@ -6,26 +6,13 @@
 //
 
 import SwiftUI
-import Clerk
 
 @main
 struct taskflow_iosApp: App {
-    @State private var clerk = Clerk.shared
     
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                   if clerk.isLoaded {
-                     ContentView()
-                   } else {
-                     ContentView()
-                   }
-                 }
-                 .environment(clerk)
-                 .task {
-                   clerk.configure(publishableKey: "pk_test_aW52aXRpbmctbWlubm93LTk2LmNsZXJrLmFjY291bnRzLmRldiQ")
-                   try? await clerk.load()
-                 }
+            ContentView()
             }
         }
     }
