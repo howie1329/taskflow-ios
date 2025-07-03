a89e271d7f72853313c6862010352fd94a68012d
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-07-03 03:08:31 -0400

    working example

---
+++ taskflow-ios/TaskViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled 2.swift
+//  taskflow-ios
+//
+//  Created by Howard Thomas on 7/2/25.
+//
+
+import Foundation
+import SwiftData
+
+@Observable
+class TaskViewModel {
+    
+    var tasks: [Tasks] = []
+    var isLoading: Bool = false
+    
+}
